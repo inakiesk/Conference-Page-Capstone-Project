f058053adce7fd9ffe312e0a3b1acfbf5da72976
inakiesk <inakieskruiz@gmail.com>
2022-08-04 17:54:44 -0500

    Add js object and images of speakers in speakers section

--- index.js
+++ index.js
@@ -13,3 +13,43 @@ const link3 = document.getElementById('link3');
     menu.classList.toggle('hidden');
   });
 });
+
+const speakersData = [
+  {
+    img: 'images/speaker01.jpeg',
+    name: 'Lindsie Kallirhoe',
+    degree: 'Winner of NYO 2021',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo ad voluptas',
+  },
+  {
+    img: 'images/speaker02.jpg',
+    name: 'Amin Piotr',
+    degree: 'Winner of NYO 2020',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo ad voluptas',
+  },
+  {
+    img: 'images/speaker03.png',
+    name: 'Tami Charisse',
+    degree: 'Upcoming talent of 2022',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo ad voluptas',
+  },
+  {
+    img: 'images/speaker04.jpg',
+    name: 'Jake Whitaker',
+    degree: 'Finalist of NYO 2020',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo ad voluptas',
+  },
+  {
+    img: 'images/speaker05.jpg',
+    name: 'Nataly Smith',
+    degree: 'Finalist of NYO 2021',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo ad voluptas',
+  },
+  {
+    img: 'images/speaker06.jpeg',
+    name: 'Jake Turner',
+    degree: 'Upcoming talent of 2022',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quo ad voluptas',
+  },
+];
+
